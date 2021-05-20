431b7c4617705de5cb72ac5b24a1787c34bf5571
Tyler <tylermccall14@yahoo.com>
2021-05-20 00:31:46 -0500

    Early Projects Learning JavaScript
    Early simple projects while learning JavaScript

---
+++ TextEditorExercise-1/script.js
@@ -0,0 +1,36 @@
+
+updateText = () =>{
+  let text = document.getElementById('text-input').value;
+  document.getElementById('text-output').innerText = text;
+}
+  
+  makeBold = (elem) =>{
+  elem.classList.toggle('active');
+  document.getElementById('text-output').classList.toggle('bold');
+}
+ 
+  makeItalic = (elem) => {
+  elem.classList.toggle('active');
+  document.getElementById('text-output').classList.toggle('italic');
+}
+
+ makeUnderline = (elem) =>{
+  elem.classList.toggle('active');
+  let formattedText = document.getElementById('text-output');
+  if(formattedText.classList.contains('underline')){
+    formattedText.classList.remove('underline');
+  }
+  else {
+    formattedText.classList.add('underline');
+  }
+}
+
+alignText = (elem, alignType) =>{
+  elem.classList.toggle('active');
+  document.getElementById('text-output').style.textAlign = alignType;
+  let buttonsList = document.getElementsByClassName('align');
+  for(let i = 0; i < buttonsList.length; i++){
+    buttonsList[i].classList.remove('active');
+  }
+  elem.classList.add('active');
+}
